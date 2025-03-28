b4661f261e6773860b62d0643110ab56a6768652
anshuman8181 <anshusettipalli@gmail.com>
2025-03-28 15:44:08 +0530

    Add files via upload

---
+++ stroperation.py
@@ -0,0 +1,5 @@
+name="anshuman settipalli"
+print(len(name))
+print(name[4])
+print(name[3:15]) 
+print(name[-1:-10])
---
+++ typecasting.py
@@ -0,0 +1,13 @@
+# num1="21"
+# num2="34"
+# print(num1+num2)
+# num1=int(num1)
+# num2=int(num2)
+# print(num1+num2) 
+name=34
+print(type(name))
+name=str(name)
+print(type(name))
+first_name="anshuman"
+last_name="settipalli"
+print(first_name+" "+last_name)
