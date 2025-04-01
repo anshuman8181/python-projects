0e91168eb71083a85b0fa8ace13468c53a99791a
anshuman8181 <anshusettipalli@gmail.com>
2025-04-01 15:16:57 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,13 @@
+
+birthday_1 = int(input("Enter birthday 1"))
+birthday_2 = int(input("Enter birthday 2"))
+birthday_3 = int(input("Enter birthday 3"))
+birthday_4 = int(input("Enter birthday 4"))
+birthday_5 = int(input("Enter birthday 5"))
+
+# Printing the birthday dates
+print("Birthday 1:", birthday_1)
+print("Birthday 2:", birthday_2)
+print("Birthday 3:", birthday_3)
+print("Birthday 4:", birthday_4)
+print("Birthday 5:", birthday_5)
