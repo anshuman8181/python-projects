959c8cbac59a8ace2d0848c498fede4c9c294edb
anshuman8181 <anshusettipalli@gmail.com>
2025-04-01 15:42:55 +0530

    Add files via upload

---
+++ congradulation.py
@@ -0,0 +1,5 @@
+
+print("Congratulations Raju! You have won a gold medal! 🎉🥇")
+message = input("Write a message for Raju: ")
+# Converting input to uppercase
+print("Your message in uppercase:", message.upper())
