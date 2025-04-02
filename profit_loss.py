96b63c665ed27e89a118826e822a31c9cacb5e5c
anshuman8181 <anshusettipalli@gmail.com>
2025-04-02 15:45:12 +0530

    Add files via upload

---
+++ profit_loss.py
@@ -0,0 +1,9 @@
+buying_price=100
+selling_price=120
+if buying_price>selling_price:
+    print("it is a loss")
+else:
+    # profit=selling_price-buying_price
+    print("it is a profit and this is the profit amount")
+    profit=selling_price-buying_price
+    print(profit)
