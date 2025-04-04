45bf7acd69cb99b052f06c2df395b1b0be59e780
anshuman8181 <anshusettipalli@gmail.com>
2025-04-04 15:49:23 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,17 @@
+num1=120
+num2=36
+num3=4
+# if num1>num2:
+#     if num1>num3:
+#         print("num1 is greater")
+# elif num2>num3:
+#     if num2>num3:
+#         print("num2 is greater")
+# else:
+#     print("num3 is greater")
+if num1>num2 and num1>num3:
+    print("num1 is greater")
+elif num2>num3 and num2>num1:
+    print("num2 is greater ")
+else:
+    print("num3 is greater ")
