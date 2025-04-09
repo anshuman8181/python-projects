595c702126590a494e6213bc4455a9a0a8de162c
anshuman8181 <anshusettipalli@gmail.com>
2025-04-09 13:24:06 +0530

    Add files via upload

---
+++ all.py
@@ -0,0 +1,17 @@
+# identity Operators
+a = 10
+b = 10
+c = "hello"
+d = "hello"
+
+print("Identity Operators:")
+print(a is b)        
+print(c is d)         
+print(a is not c)    
+
+# membership Operators
+text = "hello world"
+
+print("Membership Operators:")
+print("hello" in text)      
+print("bye" not in text)    
