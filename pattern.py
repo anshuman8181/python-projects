3e0cea42114ec2e338aaf3708deb18568023826d
anshuman8181 <anshusettipalli@gmail.com>
2025-04-21 15:45:42 +0530

    Add files via upload

--- pattern.py
+++ pattern.py
@@ -1,4 +1,20 @@
+# for i in range(5):
+#     for i in range(5):
+#         print("*",end="")
+#     print("\n")
+
+
+#right angle triange
+
+# for i in range(5):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print("\n",end="")
+
+
 for i in range(5):
-    for i in range(5):
-        print("*",end="")
-    print("\n")
+    for j in range(5,i,-1):
+        print(" ",end=" ")
+    for h in range(i):
+        print("/",end=" ")
+    print("\n",end="")
