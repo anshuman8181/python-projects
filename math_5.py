56a57f615a98ab16275af7bae49911c1d1c3bb66
anshuman8181 <anshusettipalli@gmail.com>
2025-05-14 13:00:47 +0530

    Add files via upload

---
+++ math_5.py
@@ -0,0 +1,6 @@
+import math
+
+print("Power: 3 raised to 4 is =", math.pow(3, 4))
+print("Absolute value of -27 is =", math.fabs(-27))
+print("Remainder of 17 divided by 4 is =", math.remainder(17, 4))
+print("Value of pi is =", math.pi)
