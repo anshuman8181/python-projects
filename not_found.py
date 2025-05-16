a45ea9ecccf5dd0261e5f8f1927fa377f6136972
anshuman8181 <anshusettipalli@gmail.com>
2025-05-16 15:43:57 +0530

    Add files via upload

---
+++ not_found.py
@@ -0,0 +1,12 @@
+country_code = {'India' : '0091',
+                'Aus' : '0025',
+                'Nepal' : '00977'}
+
+print("country code for India -")
+print(country_code.get("India","not found"))
+
+print("country code for Japan -")
+print(country_code.get("japan","not found"))
+
+
+
