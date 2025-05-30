951b2b9dbd5d9f5da03c41c7aeff312f5a03c935
anshuman8181 <anshusettipalli@gmail.com>
2025-05-30 15:37:32 +0530

    Add files via upload

---
+++ penguin.py
@@ -0,0 +1,30 @@
+class Bird:
+
+
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+
+    def swim(self):
+        print("swim faster")       
+
+
+class Penguin(Bird):
+
+    def __init__(self):
+        super( ).__init__()
+        print("penguin is ready")
+
+    def whoisThis(self):
+        print("Penguin")
+
+    def run(self):
+        print("Run faster")
+        
+
+peggy = Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
